e98ad396b72bf085e020a13e0683355ca1257bf1
Divyansh Ajay <74562672+divyansh-git-max@users.noreply.github.com>
2024-02-26 11:12:18 +0000

    maths.js file created

---
+++ Basics-01/05_maths.js
@@ -0,0 +1,9 @@
+console.log(Math);
+console.log(Math.abs(4))
+console.log(Math.round(1.73))
+console.log(Math.ceil(8.6));// -> upper value
+console.log(Math.floor(9.7));// -> lower value
+
+console.log(Math.min(1,2,3,4));
+console.log(Math.max(3,4,5,6));
+
