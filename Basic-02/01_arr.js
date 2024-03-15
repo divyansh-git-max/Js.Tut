4e523564571ad9c7d24d8cc4bba1542545e3058d
Divyansh Ajay <74562672+divyansh-git-max@users.noreply.github.com>
2024-03-15 17:51:48 +0000

    changes made!

---
+++ Basic-02/01_arr.js
@@ -0,0 +1,19 @@
+//array 
+
+const myArr= [0,1,2,3,4,5] // index starts at 0 
+
+
+const myHeros = ['iron man', 'hulk']
+const myArr2 = new Array(1,2)
+console.log(myArr[0])
+
+myArr.push(5)
+console.log(myArr)
+console.log(myArr2);
+myArr.pop(5)
+console.log(myArr)
+
+myArr.unshift(9)
+
+console.log(myArr);
+
