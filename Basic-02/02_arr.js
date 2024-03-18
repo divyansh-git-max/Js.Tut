6e4c479f12ea4b84b3fcaa9f8971f87e9d251337
Divyansh Ajay <74562672+divyansh-git-max@users.noreply.github.com>
2024-03-18 09:55:58 +0000

    some changes made !!!!!

--- Basic-02/02_arr.js
+++ Basic-02/02_arr.js
@@ -2,8 +2,21 @@ const marvel_heros = ['Iron Man', "Thor", 'Hulk']
 const DC_Heros= ['Batman', "Superman"] 
 const arr1 = ['Batman','Superman']
 
-marvel_heros.push(DC_Heros) // merge the array in the array 
-console.log(marvel_heros)
+// marvel_heros.push(DC_Heros) // merge the array in the array 
+// console.log(marvel_heros)
   
-marvel_heros.concat(DC_Heros)
-console.log(marvel_heros);
+// marvel_heros.concat(DC_Heros)
+// console.log(marvel_heros);
+
+
+// console.log(marvel_heros)
+
+const all_new_heros = [... marvel_heros, ...DC_Heros]
+console.log(all_new_heros);
+
+
+const another_array = [1,2,3 , [4,5,6], 7 , [6,7], [4,5]]
+const real_array = another_array.flat(Infinity)
+console.log(real_array);
+console.log(Array.isArray('Divyansh')) // -> false means it is not array vice - versa 
+
